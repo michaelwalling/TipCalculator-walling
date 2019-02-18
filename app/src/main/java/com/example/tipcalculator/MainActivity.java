4f975738d2eef2abac6c69515ab075e5e30836b0
Michael Walling <wallingm@g.cofc.edu>
2019-02-17 20:24:30 -0500

    finished design layout, added dimens file

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -2,17 +2,52 @@ package com.example.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView counter;
+    EditText totalInputNum;
+    EditText peopleNum;
 
-    private int totalAmount;
+    private double totalNum;
+    private double total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalInputNum = findViewById(R.id.totalInputNum);
+        peopleNum  = findViewById(R.id.peopleNum);
+
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.tipAmount1:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.tipAmount2:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.tipAmount3:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.customTipAmount:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
